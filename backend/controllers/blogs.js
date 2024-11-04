80e36e684ce87b5116112dc9e3ab1a6f99972eb9
Kai <kai@hellokai.dev>
2024-11-04 15:38:02 +0100

    exercises 4.8 to 4.14 (#4)
    * add tests for api 4.8-4.12
    
    * complete exercises to 4.14

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -1,21 +1,42 @@
 const blogsRouter = require('express').Router();
-const Blog = require('../models/notes');
+const Blog = require('../models/blog');
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs);
-  });
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({});
+  return response.json(blogs);
 });
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body);
 
-  blog.save().then(result => {
-    response
-      .status(201)
-      .json(result)
-      .catch(error => next(error));
+  const savedBlog = await blog.save();
+  return response.status(201).json(savedBlog);
+});
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  return response.status(204).end();
+});
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body;
+  console.log('*************in PUT', request.params.id, body);
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+    runValidators: true,
+    context: 'query',
   });
+
+  console.log('*************in PUT!!!', updatedBlog);
+  return response.json(updatedBlog);
 });
 
 module.exports = blogsRouter;
--- backend/tests/list.test.js
+++ backend/tests/list.test.js
@@ -1,60 +1,7 @@
 const { test, describe } = require('node:test');
 const assert = require('node:assert');
 const listHelper = require('../utils/list_helper');
-
-const listWithOneBlog = [
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-    likes: 5,
-    __v: 0,
-  },
-];
-
-const multipleBlogs = [
-  {
-    _id: '5a422aa71b54a676234d17f9',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-    likes: 5,
-    __v: 0,
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0,
-  },
-  {
-    _id: '5a422b3a1b54a676234d17f9',
-    title: 'Canonical string reduction',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-    likes: 12,
-    __v: 0,
-  },
-  {
-    _id: '5a422b891b54a676234d17fa',
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10,
-    __v: 0,
-  },
-  {
-    _id: '5a422ba71b54a676234d17fb',
-    title: 'TDD harms architecture',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-    likes: 10,
-    __v: 0,
-  },
-];
+const testHelper = require('./test_helper');
 
 test('dummy returns one', () => {
   const blogs = [];
@@ -65,7 +12,7 @@ test('dummy returns one', () => {
 
 describe('total likes', () => {
   test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.totalLikes(listWithOneBlog);
+    const result = listHelper.totalLikes(testHelper.listWithOneBlog);
     assert.strictEqual(result, 5);
   });
 
@@ -75,15 +22,15 @@ describe('total likes', () => {
   });
 
   test('returns the right total for multiple blogs', () => {
-    const result = listHelper.totalLikes(multipleBlogs);
+    const result = listHelper.totalLikes(testHelper.multipleBlogs);
     assert.strictEqual(result, 42);
   });
 });
 
 describe('favorite blogs', () => {
   test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.favoriteBlog(listWithOneBlog);
-    assert.deepStrictEqual(result, listWithOneBlog[0]);
+    const result = listHelper.favoriteBlog(testHelper.listWithOneBlog);
+    assert.deepStrictEqual(result, testHelper.listWithOneBlog[0]);
   });
 
   test('should return an empty {} if the blog list is empty', () => {
@@ -92,14 +39,14 @@ describe('favorite blogs', () => {
   });
 
   test('should return the first blog in the list with the highest likes if there are multiple with the same likes', () => {
-    const result = listHelper.favoriteBlog(multipleBlogs);
-    assert.deepStrictEqual(result, multipleBlogs[2]);
+    const result = listHelper.favoriteBlog(testHelper.multipleBlogs);
+    assert.deepStrictEqual(result, testHelper.multipleBlogs[2]);
   });
 });
 
 describe('most blogs', () => {
   test('when list has only one blog, equals the likes of that', () => {
-    const result = listHelper.mostBlogs(listWithOneBlog);
+    const result = listHelper.mostBlogs(testHelper.listWithOneBlog);
     assert.deepStrictEqual(result, {
       author: 'Edsger W. Dijkstra',
       blogs: 1,
@@ -112,7 +59,7 @@ describe('most blogs', () => {
   });
 
   test('should return the first author when there are multiple authors with the same number of blogs', () => {
-    const result = listHelper.mostBlogs(multipleBlogs);
+    const result = listHelper.mostBlogs(testHelper.multipleBlogs);
     assert.deepStrictEqual(result, {
       author: 'Edsger W. Dijkstra',
       blogs: 3,
@@ -121,21 +68,21 @@ describe('most blogs', () => {
 });
 
 describe('most likes', () => {
-  // test('when list has only one blog, equals the likes of that', () => {
-  //   const result = listHelper.mostLikes(listWithOneBlog);
-  //   assert.deepStrictEqual(result, {
-  //     author: 'Edsger W. Dijkstra',
-  //     likes: 5,
-  //   });
-  // });
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.mostLikes(testHelper.listWithOneBlog);
+    assert.deepStrictEqual(result, {
+      author: 'Edsger W. Dijkstra',
+      likes: 5,
+    });
+  });
 
-  // test('should return an empty {} if the blog list is empty', () => {
-  //   const result = listHelper.mostLikes([]);
-  //   assert.deepStrictEqual(result, {});
-  // });
+  test('should return an empty {} if the blog list is empty', () => {
+    const result = listHelper.mostLikes([]);
+    assert.deepStrictEqual(result, {});
+  });
 
   test('should return the author with most likes and the total number of likes', () => {
-    const result = listHelper.mostLikes(multipleBlogs);
+    const result = listHelper.mostLikes(testHelper.multipleBlogs);
     assert.deepStrictEqual(result, {
       author: 'Edsger W. Dijkstra',
       likes: 22,
