423a597b45dbcfcab338479f570aaf52aced3265
Kai <kai@hellokai.dev>
2024-11-07 17:56:51 +0100

    implement frontend, exercies 5.1 to 5.3 (#6)

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ mongoose
   });
 
 app.use(cors());
-// app.use(express.static('dist'));
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
---
+++ bloglist-frontend/src/components/Login.jsx
@@ -0,0 +1,36 @@
+const Login = ({
+  handleLogin,
+  username,
+  setUsername,
+  password,
+  setPassword,
+}) => {
+  return (
+    <>
+      <h2>log in to applicaton</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
